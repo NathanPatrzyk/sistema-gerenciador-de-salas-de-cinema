e95857ffb45ec6a212081eb740a2585f4e53347d
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-04-25 21:15:20 -0300

    ✨ feat(tb_diretor): Criando tb_diretor

--- create.sql
+++ create.sql
@@ -4,8 +4,16 @@ USE db_cinema;
 --
 CREATE TABLE tb_sala(
     numero_sala INT NOT NULL,
-    descricao_sala CHAR(20) NOT NULL,
+    descricao_sala VARCHAR(20) NOT NULL,
     capacidade_sala INT NOT NULL,
     CONSTRAINT pk_tb_sala PRIMARY KEY (numero_sala)
-    ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=INNODB;
+DESC tb_sala;
+--
+CREATE TABLE tb_diretor(
+    codigo_diretor INT NOT NULL,
+    nome_diretor VARCHAR(20) NOT NULL,
+    CONSTRAINT pk_tb_diretor PRIMARY KEY (codigo_diretor)
+)ENGINE=INNODB;
+DESC tb_diretor;
+--

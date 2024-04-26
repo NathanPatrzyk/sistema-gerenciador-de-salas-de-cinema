157cc5b9907129557696f7aeaed78e6754b6d87d
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-04-26 16:53:23 -0300

    ✨ feat(tb_diretor): INSERT INTO tb_diretor

---
+++ insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_diretor
+    (codigo_diretor, nome_diretor)
+VALUES
+    (1, "Pedro Paulo Matos"),
+    (2, "Jorge da Penha"),
+    (3, "Severino Juca"),
+    (4, "Tim Burton"),
+    (5, "Rob Zombie"),
+    (6, "Quentin Tarantino");

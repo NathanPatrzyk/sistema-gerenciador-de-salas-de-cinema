6c09102d1a211eb5c89bb736f5d74b824255066a
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-04-25 21:05:08 -0300

    Revert "✨ feat(tb_sala): Criando tb_sala"
    This reverts commit dc2f1b1b20df157540a18dae749c9eb77eb6e3c5.

--- create.sql
+++ create.sql
@@ -1,11 +1,3 @@
 DROP DATABASE IF EXISTS db_cinema;
 CREATE DATABASE db_cinema;
 USE db_cinema;
---
-CREATE TABLE tb_sala(
-    numero_sala INT NOT NULL,
-    descricao_sala CHAR(20) NOT NULL,
-    capacidade_sala INT NOT NULL,
-    CONSTRAINT pk_tb_sala PRIMARY KEY (numero_sala)
-    ON DELETE CASCADE ON UPDATE CASCADE
-)ENGINE=INNODB;

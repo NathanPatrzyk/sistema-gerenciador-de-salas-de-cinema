6913784bb4425dd316129062ad7311fcc1a56ccc
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-04-27 16:35:24 -0300

    ✨ feat(tb_filme): Teste update tb_filme

--- update.sql
+++ update.sql
@@ -1,1 +1,3 @@
 -- No momento da inserção de dados da tbFilme deixe a categoria_filme em branco. Depois que todas as tabelas foram preenchidas utilizando o comando UPDATE atualize a categoria de filmes de 20 em 20 com as seguintes categorias “Terror”, “Comédia”, “Ação”, ”Suspense”, “Romance”.
+SELECT * FROM tb_filme
+WHERE codigo_filme < 20;
